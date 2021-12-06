fcde2224e260edab0879dbcf3b843cdfc757c03d
kingford <32887862+skingford@users.noreply.github.com>
2021-12-06 19:54:19 +0800

    feat: micro desc

--- micro-app-vite-vue/vite.config.ts
+++ micro-app-vite-vue/vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-12-06 19:22:55
- * @LastEditTime: 2021-12-06 19:52:41
+ * @LastEditTime: 2021-12-06 19:53:56
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -9,7 +9,11 @@ import qiankun from "vite-plugin-qiankun";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), qiankun("ViteVueMicroApp", { useDevMode: true })],
+  plugins: [
+    vue(),
+    // ViteVueMicroApp子应用名，主应用注册时AppName需保持一致
+    qiankun("ViteVueMicroApp", { useDevMode: true }),
+  ],
   // 生产环境需要指定运行域名作为base
   //base: 'http://xxx.com/',
   server: {
