18fa84fa1b7a95e5d4da7cde92740c70656097e6
kingford <32887862+skingford@users.noreply.github.com>
2021-12-08 16:07:56 +0800

    feat: 路由

--- micro-app-main/src/router/routes/index.ts
+++ micro-app-main/src/router/routes/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-12-08 11:52:05
- * @LastEditTime: 2021-12-08 15:35:46
+ * @LastEditTime: 2021-12-08 15:59:23
  */
 import {
   RootRoute,
@@ -9,6 +9,7 @@ import {
   REDIRECT_ROUTE,
   ERROR_LOG_ROUTE,
   routeModuleList,
+  // PAGE_NOT_FOUND_ROUTE, // 微前端主应用不需要404页面
 } from "./base";
 
 export const asyncRoutes = [...routeModuleList];
